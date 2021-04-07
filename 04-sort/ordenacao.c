873074bb9c20ebea9e4f7a1c54833fe3778b29af
mjbrusso <mjbrusso@gmail.com>
2021-04-07 14:18:43 -0300

    Update sort

--- 04-sort/ordenacao.c
+++ 04-sort/ordenacao.c
@@ -22,10 +22,35 @@ void bubblesort(int vet[], int n)
 
 void selectionsort(int vet[], int n)
 {
-    
+    while (n > 1)
+    {
+        int posm = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (vet[i] > vet[posm])
+                posm = i;
+        }
+        n--;
+        if (posm != n)
+        {
+            int aux = vet[posm];
+            vet[posm] = vet[n];
+            vet[n] = aux;
+        }
+    }
 }
 
 void insertionsort(int vet[], int n)
 {
-
+    for (int i = 1; i < n; ++i)
+    {
+        int aux = vet[i];
+        int j = i - 1;
+        while (j >= 0 && vet[j] > aux)
+        {
+            vet[j + 1] = vet[j];
+            j--;
+        }
+        vet[j + 1] = aux;
+    }
 }
